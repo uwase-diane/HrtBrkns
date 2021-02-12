323e1db1114abc48047fb07c3a84928817365696
uwase-diane <diane.uwase13@gmail.com>
2021-02-12 03:04:32 -0500

    admin site

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from.models import Project, Profile, Comments
+
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Comments)
